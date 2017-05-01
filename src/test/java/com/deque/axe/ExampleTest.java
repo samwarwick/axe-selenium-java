c870d26e3ae01140fddbe95fff732881af0dc23a
Sam Warwick <sam@samwarwick.com>
2017-05-02 09:54:15 +1000

    Default to Chrome instead of Firefox

--- src/test/java/com/deque/axe/ExampleTest.java
+++ src/test/java/com/deque/axe/ExampleTest.java
@@ -23,7 +23,7 @@ import org.junit.Test;
 import org.junit.rules.TestName;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.net.URL;
 
@@ -40,7 +40,7 @@ public class ExampleTest {
 	 */
 	@Before
 	public void setUp() {
-		driver = new FirefoxDriver();
+		driver = new ChromeDriver();
 
 		driver.get("http://localhost:5005");
 	}
